e28506b1984d89a0fb7018d28137ed0f94955b89
Rahat ( umm-arwa) <124298703+Rahat25252@users.noreply.github.com>
2023-04-05 08:29:55 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+// alert("hello world");
+
+// var myName="mark"
+
+// alert("Error!Please enter a valid Password");
+
+// alert("Welcome to JS Land..\nHappy coding!");
+
+// alert("Welcome to JS Land...");
+
+// alert("Happy Coding!\n Prevent this page from creating additional dialogs");
+
+// alert("Hello...i can run js through my web browers console");
